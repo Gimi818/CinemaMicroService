0c42e274fba7a801bebff6185a19ae3a126d84f3
Gimi818 <gimi8183@gmail.com>
2023-12-06 15:55:20 +0100

    Creates microservice Film

---
+++ film/src/test/java/com/service/film/FilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.film;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
