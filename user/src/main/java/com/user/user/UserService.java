81afb2a4711cdea9e20d8247f36f71245c3de25a
Gimi818 <gimi8183@gmail.com>
2023-12-06 14:27:51 +0100

    Created UserService

---
+++ user/src/main/java/com/user/user/UserService.java
@@ -0,0 +1,16 @@
+package com.user.user;
+
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+@AllArgsConstructor
+@Log4j2
+public class UserService {
+
+
+}
