a2425840dc230a6fce6caa70f172be88254b9d05
Gimi818 <gimi8183@gmail.com>
2023-12-06 14:34:37 +0100

    Created AlreadyExistException

---
+++ user/src/main/java/com/user/user/exception/exceptions/AlreadyExistException.java
@@ -0,0 +1,8 @@
+package com.user.user.exception.exceptions;
+
+public class AlreadyExistException extends RuntimeException {
+    public AlreadyExistException(final String message, final Object... args) {
+        super(String.format(message, args));
+    }
+}
+
