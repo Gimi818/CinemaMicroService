4fb361c6c16b9493105cf8041cdd97135f935716
Gimi818 <gimi8183@gmail.com>
2023-12-06 14:37:19 +0100

    Created UserController

---
+++ user/src/main/java/com/user/user/UserController.java
@@ -0,0 +1,14 @@
+package com.user.user;
+
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+}
