b94f9d6beef468e941d63372841f8d8997d166e6
Gimi818 <gimi8183@gmail.com>
2023-12-06 14:28:59 +0100

    Created UserRepository

---
+++ user/src/main/java/com/user/user/UserRepository.java
@@ -0,0 +1,12 @@
+package com.user.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    boolean existsByEmail(String email);
+    Optional<User> findByUuid(UUID userUuid);
+    Optional<User> findByConfirmationToken(String confirmationToken);
+}
