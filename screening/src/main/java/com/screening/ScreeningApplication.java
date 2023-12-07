0df65c20d71c739dfe91001528716ffc9a08a3a5
Gimi818 <gimi8183@gmail.com>
2023-12-07 10:42:54 +0100

    Configuration

---
+++ screening/src/main/java/com/screening/ScreeningApplication.java
@@ -0,0 +1,13 @@
+package com.screening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreeningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreeningApplication.class, args);
+	}
+
+}
