207dfc07c07240d9784da6cafec002e8e9995963
Gimi818 <gimi8183@gmail.com>
2023-12-07 10:38:53 +0100

    Created TooLateException

---
+++ screening/src/main/java/com/screening/exception/exceptions/TooLateException.java
@@ -0,0 +1,10 @@
+package com.screening.exception.exceptions;
+
+public class TooLateException extends RuntimeException {
+
+    public TooLateException(final String message) {
+        super(message);
+    }
+
+}
+
