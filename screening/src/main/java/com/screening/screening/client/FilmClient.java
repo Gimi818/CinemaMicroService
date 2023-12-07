c68d33c429048c015284433271d1ea95fcd4f8e9
Gimi818 <gimi8183@gmail.com>
2023-12-07 21:16:46 +0100

    Created FilmClient

---
+++ screening/src/main/java/com/screening/screening/client/FilmClient.java
@@ -0,0 +1,23 @@
+package com.screening.screening.client;
+
+import com.screening.screening.Film;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+@FeignClient(name ="film-service", url = "${application.config.films-url}")
+public interface FilmClient {
+
+    @GetMapping("{id}")
+    ResponseEntity<Film> findById(@PathVariable ("id") Long id);
+
+    @GetMapping("{id}")
+    Film findFilmById(@PathVariable ("id") Long id);
+
+}
+
