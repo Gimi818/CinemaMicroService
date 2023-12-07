4c580ef9d51d464aaa800f78a8ed1973528e945e
Gimi818 <gimi8183@gmail.com>
2023-12-07 10:41:52 +0100

    Created NotFoundException

---
+++ screening/src/main/java/com/screening/exception/exceptions/NotFoundException.java
@@ -0,0 +1,15 @@
+package com.screening.exception.exceptions;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(final String message) {
+        super(message);
+    }
+    public NotFoundException(final Long id) {
+        super(String.format("No entity with UUID: %s", id));
+    }
+
+    public NotFoundException(final String message, final Object... args) {
+        super(String.format(message, args));
+    }
+
+}
