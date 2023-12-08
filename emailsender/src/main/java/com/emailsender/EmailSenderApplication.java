823a8900883c11885a77b5932da315dbb13440d2
Gimi818 <gimi8183@gmail.com>
2023-12-08 20:31:31 +0100

    Created EmailSenderApplication

---
+++ emailsender/src/main/java/com/emailsender/EmailSenderApplication.java
@@ -0,0 +1,13 @@
+package com.emailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSenderApplication.class, args);
+	}
+
+}
