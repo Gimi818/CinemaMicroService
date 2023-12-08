672c2ae4bc8f317c23495409c91ae86dc50208aa
Gimi818 <gimi8183@gmail.com>
2023-12-08 20:31:42 +0100

    Created EmailSenderApplication

---
+++ emailsender/src/test/java/com/emailsender/EmailsenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailsenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
