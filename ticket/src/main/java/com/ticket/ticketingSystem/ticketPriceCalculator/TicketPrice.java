a1e489cceb131af74bfe1caf40e3c616de769df9
Gimi818 <gimi8183@gmail.com>
2023-12-17 19:30:00 +0100

    Created TicketPriceCalculator

---
+++ ticket/src/main/java/com/ticket/ticketingSystem/ticketPriceCalculator/TicketPrice.java
@@ -0,0 +1,11 @@
+package com.ticket.ticketingSystem.ticketPriceCalculator;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TicketPrice {
+    public static final int TICKET_PRICE_WITH_EVENT_REDUCE = 20;
+    public static final int BASIC_TICKET_PRICE = 28;
+    public static final int STUDENT_REDUCE = 5;
+}
