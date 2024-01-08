b7c6538222fead7e98305829ef4ee25dbd8f9ae1
Gimi818 <gimi8183@gmail.com>
2024-01-08 11:25:20 +0100

    Added NotFoundException to ErrorHandler

--- ticket/src/main/java/com/ticket/common/exception/ControllerErrorHandler.java
+++ ticket/src/main/java/com/ticket/common/exception/ControllerErrorHandler.java
@@ -1,7 +1,9 @@
 package com.ticket.common.exception;
 
+import com.ticket.common.exception.exceptions.NotFoundException;
 import com.ticket.common.exception.exceptions.TooLateToBookException;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,4 +21,16 @@ public class ControllerErrorHandler {
         log.error(message);
         return new
                 ErrorResponse(message, HttpStatus.CONFLICT);
-    }}
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseBody
+    public ErrorResponse notFound(NotFoundException exception) {
+        final String message = exception.getMessage();
+        log.error(message);
+        return new
+                ErrorResponse(message, HttpStatus.NOT_FOUND);
+    }
+
+}
