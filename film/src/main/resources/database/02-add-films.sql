f037c86add7b77126c65f57fe2115cf56186916f
Gimi818 <gimi8183@gmail.com>
2024-01-09 13:58:30 +0100

    Added 02-add-films.sql

---
+++ film/src/main/resources/database/02-add-films.sql
@@ -0,0 +1,29 @@
+--liquibase formatted sql
+--changeset wgmiterek:2
+insert into film(title, category, duration_film_in_minutes) values ('TOP GUN', 'ACTION', 140);
+insert into film(title, category, duration_film_in_minutes) values ('JOHN WICK 4', 'HORROR', 167);
+insert into film(title, category, duration_film_in_minutes) values ('The Father', 'DRAMA', 122);
+insert into film(title, category, duration_film_in_minutes) values ('Mission: Impossible – Dead Reckoning', 'ACTION', 164);
+insert into film(title, category, duration_film_in_minutes) values ('NOWHERE', 'DRAMA', 115);
+insert into film(title, category, duration_film_in_minutes) values ('The Kill Room', 'ACTION', 133);
+insert into film(title, category, duration_film_in_minutes) values ('The Exorcist: Believer', 'HORROR', 125);
+insert into film(title, category, duration_film_in_minutes) values ('A Haunting In Venice', 'HORROR', 133);
+insert into film(title, category, duration_film_in_minutes) values ('Indiana Jones and the Dial of Destiny', 'ACTION', 142);
+insert into film(title, category, duration_film_in_minutes) values ('Operation Fortune: Ruse de guerre ', 'COMEDY', 114);
+insert into film(title, category, duration_film_in_minutes) values ('THE MOTHER', 'THRILLER', 118);
+insert into film(title, category, duration_film_in_minutes) values ('Luther: The Fallen Sun', 'THRILLER', 129);
+insert into film(title, category, duration_film_in_minutes) values ('Transformers: Rise of the Beasts', 'FANTASY', 127);
+insert into film(title, category, duration_film_in_minutes) values ('The Gray Man', 'ACTION', 122);
+insert into film(title, category, duration_film_in_minutes) values ('ABOUT FATE', 'COMEDY', 104);
+insert into film(title, category, duration_film_in_minutes) values ('MEN', 'HORROR', 100);
+insert into film(title, category, duration_film_in_minutes) values ('The Good Nurse', 'THRILLER', 134);
+insert into film(title, category, duration_film_in_minutes) values ('The School for Good', 'FANTASY', 141);
+insert into film(title, category, duration_film_in_minutes) values ('NO EXIT', 'HORROR', 97);
+insert into film(title, category, duration_film_in_minutes) values ('Predator: Prey', 'ACTION', 100);
+insert into film(title, category, duration_film_in_minutes) values ('MENU', 'HORROR', 111);
+insert into film(title, category, duration_film_in_minutes) values ('Lou', 'DRAMA', 119);
+insert into film(title, category, duration_film_in_minutes) values ('Morbius', 'FANTASY', 124);
+insert into film(title, category, duration_film_in_minutes) values ('The Man From Toronto', 'COMEDY', 135);
+insert into film(title, category, duration_film_in_minutes) values ('Uncharted', 'ACTION', 116);
+
+
