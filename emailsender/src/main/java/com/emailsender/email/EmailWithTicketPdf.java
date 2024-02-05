039f4e6748faa8dd5c81341c5702b83ee8148b69
Gimi818 <gimi8183@gmail.com>
2024-02-05 12:30:03 +0100

    Code refactoring, splitting a single large sendEmailWithPDF method into 7 smaller ones

--- emailsender/src/main/java/com/emailsender/email/EmailWithTicketPdf.java
+++ emailsender/src/main/java/com/emailsender/email/EmailWithTicketPdf.java
@@ -13,6 +13,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Locale;
 import java.util.ResourceBundle;
@@ -24,65 +25,74 @@ public class EmailWithTicketPdf {
     private final GenerateQrCode generateQrCode;
 
 
-    public void sendEmailWithPDF( EmailWithTicket data) throws MessagingException {
+    public void sendEmailWithPDF(EmailWithTicket data) throws MessagingException {
+        ByteArrayOutputStream pdfOutputStream = new ByteArrayOutputStream();
+        Document document = prepareDocument(data, pdfOutputStream);
+        byte[] pdfBytes = pdfOutputStream.toByteArray();
+        sendEmail(data.email(), pdfBytes);
+    }
+
+    private Document prepareDocument(EmailWithTicket data, ByteArrayOutputStream pdfOutputStream) {
         ResourceBundle bundle = ResourceBundle.getBundle("messages", Locale.getDefault());
         Document document = new Document();
-        String logoPath = "classpath:logo2.png";
-        ByteArrayOutputStream pdfOutputStream = new ByteArrayOutputStream();
         try {
-            PdfWriter writer = PdfWriter.getInstance(document, pdfOutputStream);
+            PdfWriter.getInstance(document, pdfOutputStream);
             document.open();
-
-            Image logo = Image.getInstance(logoPath);
-            logo.scaleToFit(600, 200);
-            logo.setSpacingBefore(0);
-            logo.setAlignment(Element.ALIGN_CENTER);
-
-            document.add(logo);
-            Font titleFont = new Font(Font.FontFamily.HELVETICA, 25, Font.NORMAL, BaseColor.BLACK);
-            Paragraph filmTitle = new Paragraph(data.ticketDto().filmTitle(), titleFont);
-            filmTitle.setAlignment(Element.ALIGN_CENTER);
-
-            Font dataFont = new Font(Font.FontFamily.HELVETICA, 20, Font.NORMAL, BaseColor.BLACK);
-            Paragraph filmData = new Paragraph(bundle.getString("ticket.date") + data.ticketDto().screeningDate(), dataFont);
-            filmData.setAlignment(Element.ALIGN_LEFT);
-
-            Paragraph filmTime = new Paragraph(bundle.getString("ticket.time") + data.ticketDto().screeningTime(), dataFont);
-            filmTime.setAlignment(Element.ALIGN_LEFT);
-
-
-            document.add(filmTitle);
-            document.add(filmData);
-            document.add(filmTime);
-            document.add(new Paragraph(bundle.getString("ticket.name") + data.ticketDto().name()));
-            document.add(new Paragraph(bundle.getString("ticket.roomNumber") + data.ticketDto().roomNumber()));
-            document.add(new Paragraph(bundle.getString("ticket.row") + data.ticketDto().rowsNumber()));
-            document.add(new Paragraph(bundle.getString("ticket.seat") + data.ticketDto().seatInRow()));
-            document.add(new Paragraph(bundle.getString("ticket.ticketType") + data.ticketDto().ticketType()));
-            document.add(new Paragraph(bundle.getString("ticket.ticketPrice") + data.ticketDto().ticketPrice() + " " + data.ticketDto().currency().toString()));
-            document.add(new Paragraph(bundle.getString("ticket.email") + data.email()));
-            document.add(new Paragraph(bundle.getString("ticket.purchaseDate") + LocalDate.now()));
-
-
-            Image qrCodeImage = (Image) generateQrCode.createQr(data.email(), data.ticketDto());
-            qrCodeImage.setAlignment(Element.ALIGN_CENTER);
-            qrCodeImage.scaleToFit(280, 280);
-            document.add(qrCodeImage);
-            document.close();
+            addLogo(document);
+            addContent(document, bundle, data);
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            document.close();
         }
+        return document;
+    }
 
+    private void addLogo(Document document) throws DocumentException, IOException {
+        String logoPath = "classpath:logo2.png";
+        Image logo = Image.getInstance(logoPath);
+        logo.scaleToFit(600, 200);
+        logo.setAlignment(Element.ALIGN_CENTER);
+        document.add(logo);
+    }
+
+    private void addContent(Document document, ResourceBundle bundle, EmailWithTicket data) throws DocumentException, IOException {
+        addParagraph(document, data.ticketDto().filmTitle(), 25, Element.ALIGN_CENTER);
+        addParagraph(document, bundle.getString("ticket.date") + data.ticketDto().screeningDate(), 20, Element.ALIGN_LEFT);
+        addParagraph(document, bundle.getString("ticket.time") + data.ticketDto().screeningTime(), 20, Element.ALIGN_LEFT);
+        document.add(new Paragraph(bundle.getString("ticket.name") + data.ticketDto().name()));
+        document.add(new Paragraph(bundle.getString("ticket.roomNumber") + data.ticketDto().roomNumber()));
+        document.add(new Paragraph(bundle.getString("ticket.row") + data.ticketDto().rowsNumber()));
+        document.add(new Paragraph(bundle.getString("ticket.seat") + data.ticketDto().seatInRow()));
+        document.add(new Paragraph(bundle.getString("ticket.ticketType") + data.ticketDto().ticketType()));
+        document.add(new Paragraph(bundle.getString("ticket.ticketPrice") + data.ticketDto().ticketPrice() + " " + data.ticketDto().currency().toString()));
+        document.add(new Paragraph(bundle.getString("ticket.email") + data.email()));
+        document.add(new Paragraph(bundle.getString("ticket.purchaseDate") + LocalDate.now()));
+        addQrCode(document, data);
+    }
+
+    private void addParagraph(Document document, String text, int fontSize, int alignment) throws DocumentException {
+        Font font = new Font(Font.FontFamily.HELVETICA, fontSize, Font.NORMAL, BaseColor.BLACK);
+        Paragraph paragraph = new Paragraph(text, font);
+        paragraph.setAlignment(alignment);
+        document.add(paragraph);
+    }
+
+    private void addQrCode(Document document, EmailWithTicket data) throws DocumentException, IOException {
+        Image qrCodeImage = (Image) generateQrCode.createQr(data.email(), data.ticketDto());
+        qrCodeImage.setAlignment(Element.ALIGN_CENTER);
+        qrCodeImage.scaleToFit(280, 280);
+        document.add(qrCodeImage);
+    }
+
+    private void sendEmail(String recipientEmail, byte[] pdfBytes) throws MessagingException {
+        ResourceBundle bundle = ResourceBundle.getBundle("messages", Locale.getDefault());
         MimeMessage message = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
-        helper.setTo(data.email());
+        helper.setTo(recipientEmail);
         helper.setSubject(bundle.getString("email.subject"));
-
-        String text = bundle.getString("email.thankYouMessage");
-        helper.setText(text, true);
-
-        byte[] pdfBytes = pdfOutputStream.toByteArray();
+        helper.setText(bundle.getString("email.thankYouMessage"), true);
         helper.addAttachment("Ticket.pdf", new ByteArrayResource(pdfBytes));
 
         javaMailSender.send(message);
