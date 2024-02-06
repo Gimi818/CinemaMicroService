f7cb9732a1621dee648f33fa873dbfe26449b0e0
Gimi818 <gimi8183@gmail.com>
2024-02-06 13:29:03 +0100

    Added @EnableKafka annotation

--- emailsender/src/main/java/com/emailsender/EmailSenderApplication.java
+++ emailsender/src/main/java/com/emailsender/EmailSenderApplication.java
@@ -2,8 +2,10 @@ package com.emailsender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class EmailSenderApplication {
 
 	public static void main(String[] args) {
--- user/src/main/java/com/user/UserApplication.java
+++ user/src/main/java/com/user/UserApplication.java
@@ -3,9 +3,11 @@ package com.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableKafka
 public class UserApplication {
 
 	public static void main(String[] args) {
