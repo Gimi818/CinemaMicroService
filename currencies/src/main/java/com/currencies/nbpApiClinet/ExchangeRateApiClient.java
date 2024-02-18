73c9bb550c7de81f33862e92ec2669ce4ed75f01
Gimi818 <gimi8183@gmail.com>
2024-02-18 18:50:10 +0100

    Created ExchangeRateApiClient class and added fetchCurrencyRatesFromNBP method

---
+++ currencies/src/main/java/com/currencies/nbpApiClinet/ExchangeRateApiClient.java
@@ -0,0 +1,22 @@
+package com.currencies.nbpApiClinet;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+
+@Service
+@Log4j2
+public class ExchangeRateApiClient {
+
+    @Value("${nbp.api.url}")
+    private String url;
+
+    public String fetchCurrencyRatesFromNBP() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        log.info("Fetched currency rates from NBP");
+        return response.getBody();
+    }
+}
+
