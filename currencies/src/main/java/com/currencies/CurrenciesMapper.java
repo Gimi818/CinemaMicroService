f3942735fd70a278f3b4a156c357378388d9d811
Gimi818 <gimi8183@gmail.com>
2024-02-18 18:50:43 +0100

    Added new mapper to CurrenciesMapper

--- currencies/src/main/java/com/currencies/CurrenciesMapper.java
+++ currencies/src/main/java/com/currencies/CurrenciesMapper.java
@@ -1,9 +1,13 @@
 package com.currencies;
 
+import com.currencies.dto.ExchangeRateDto;
 import com.currencies.dto.ExchangeRateResponseDto;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface CurrenciesMapper {
-    ExchangeRateResponseDto entityToDto(ExchangeRate exchangeRate);
+    ExchangeRateResponseDto entityToResponseDto(ExchangeRate exchangeRate);
+
+    ExchangeRateDto entityToExchangeRateDto(ExchangeRate exchangeRate);
+
 }
